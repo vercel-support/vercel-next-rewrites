d0a1be30fac2a4456138652b3c7cbc727d63e3c8
Miguel Navarro <miguelnavarro.co@gmail.com>
2023-11-21 01:45:42 -0500

    header set

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ export default async function middleware(request: NextRequest) {
   }
 
   const headers = new Headers(request.headers);
-  headers.set('host', 'www.dialpadbeta.com');
+  headers.set('x-forwarded-host', 'www.dialpadbeta.com');
 
   const proxyUrl = `${FALLBACK_SITE}/${request.nextUrl.locale}${request.nextUrl.pathname}`;
   console.log(proxyUrl);

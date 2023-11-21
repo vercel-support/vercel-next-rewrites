7e4f2626d7bdbf3cf847cc88d3466aade6b8b808
Miguel Navarro <miguelnavarro.co@gmail.com>
2023-11-21 01:42:17 -0500

    add DE

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const FALLBACK_SITE = 'https://nodejs--miguelnavarro8.repl.co';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
-    locales: ['us', 'uk', 'ca', 'au', 'jp', 'fr'],
+    locales: ['us', 'uk', 'ca', 'au', 'jp', 'fr', 'de'],
     defaultLocale: 'us',
     localeDetection: false,
   },
